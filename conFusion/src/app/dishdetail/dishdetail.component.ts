8eb5449100398cbdf691bef82cbe4dedbc3911c9
Mario Beck <marioisbeck@gmail.com>
2018-06-08 15:27:21 +0200

    Animations Part 1 - does not work because prevnext buttons don't work

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -8,17 +8,33 @@ import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
 
+import {trigger, state, style, animate, transition } from '@angular/animations';
+
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations: [
+    trigger('visibility', [
+      state('shown', style ({
+        transform: 'scale(1.0)',
+        opacity: 1
+      })),
+      state('hidden', style({
+        transform: 'scale(0.5)',
+        opacity: 0
+      })),
+      transition('* => *', animate('0.5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
   dishcopy = null;
   errMess: string;
+  visibility = 'shown';
 
   commentForm: FormGroup;
   comment: Comment;
@@ -62,8 +78,11 @@ export class DishdetailComponent implements OnInit {
 
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+      .switchMap((params: Params) => { 
+        this.visibility = 'hidden'; 
+        return this.dishservice.getDish(+params['id']);
+      })
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility = 'shown'; },
       errmess => this.errMess = <any>errmess);
   }
 
