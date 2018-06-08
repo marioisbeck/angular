7d58f18fc0e8820da074efbd7dd59284d391f5dd
Mario Beck <marioisbeck@gmail.com>
2018-06-08 16:07:10 +0200

    Animations Part 2

--- conFusion/src/app/about/about.component.ts
+++ conFusion/src/app/about/about.component.ts
@@ -2,11 +2,21 @@ import { Component, OnInit, Inject } from '@angular/core';
 
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
+import { flyInOut, expand } from '../animations/app.animations';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;1'
+
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class AboutComponent implements OnInit {
 
--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -8,7 +8,8 @@ import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
 
-import {trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility } from '../animations/app.animations';
+import { flyInOut, expand } from '../animations/app.animations';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -16,18 +17,14 @@ import 'rxjs/add/operator/switchMap';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;1'
+  },
   animations: [
-    trigger('visibility', [
-      state('shown', style ({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(),
+    visibility(),
+    expand()
   ]
 })
 export class DishdetailComponent implements OnInit {
